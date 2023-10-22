b11de7b86f76b37a27008189ac7a7d1579221974
Kevin <kevin005005@gmail.com>
2023-10-22 20:31:15 +0200

    Exercises 1.9-1.11 added

---
+++ Chapter 1 - Tutorial Introduction/Exercise 1-10.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Count words per line
+
+main()
+{
+    int character;
+    bool inWord = false;
+    int numberOfWords = 0;
+    while ((character = getchar()) != EOF) {
+        if (character == ' ' || character == '\t' || character == '\n') {
+            if (inWord) {
+                numberOfWords++;
+            }            
+            inWord = false;
+        } else {
+            inWord = true;
+        }
+        
+        if (character == '\n')
+        {
+            printf("This line contained %d words\n", numberOfWords);
+            numberOfWords = 0;
+        }
+        
+    }
+}
---
+++ Chapter 1 - Tutorial Introduction/Exercise 1-11.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Revised word counter program
+// Only character sequences that start with an English letter count as a word
+
+bool isBlank(int character) {
+    return character == ' ' || character == '\t' || character == '\n';
+}
+
+bool isLetter(int character) {
+    return ('A' <= character && character <= 'Z') || ('a' <= character && character <= 'z');
+}
+
+main()
+{
+    int character;
+    int wordCount = 0;
+    bool inWord = false;
+    bool skipSequence = false; // Skip sequence if it starts with a non-letter character
+
+    while ((character = getchar()) != EOF) {
+        if (inWord && isBlank(character)) {
+            skipSequence = false;
+            inWord = false;
+        } else if (!inWord && isLetter(character) && !skipSequence) {
+            inWord = true;
+            wordCount++;
+        } else if (!inWord && !isLetter(character)) {
+            skipSequence = true;
+        }
+    }
+    printf("The number of words is: %d", wordCount);
+}
